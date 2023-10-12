d325c50f8822d4db3569cab8fc9efcf00bbcfcfd
akash maurya <akashmauryakld97@gmail.com>
2023-10-13 04:13:54 +0530

    hi

---
+++ my-app/src/About .js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About () {
+  return (
+    <div>
+     <p>this is my first job as a software engineer</p>   
+   </div>
+  )
+}
+
+export default About 
---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import About from './About '
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Akash</h1>
+      <About/>
+    </div>
+  );
+}
+
+export default App;
